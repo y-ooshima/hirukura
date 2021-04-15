dafad035c96ca361f6a34eef70b4b07fec347477
y-ooshima <gozira.1kawa@gmail.com>
2021-04-16 00:17:16 +0900

    エディット画面の修正

--- resources/views/review/edit.blade.php
+++ resources/views/review/edit.blade.php
@@ -2,76 +2,72 @@
 
 @section('content')
 
-    <div clazss="row">
-            <nav class="navbar bg-primary">
-                <div class="d-flex align-items-center"> 
-                    <div class="navbar-nav">
-                        <ul class="nav mx-5">
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ url('/show') }}">レビュー投稿一覧へ</a>
-                            </li>
-                        </ul>
-                    </div>
+    <div class="row bg-primary">
+        <div class="col-12 my-3">
+            <div class="d-flex align-items-center">
+                <div class="ml-3">
+                    <a class="btn btn-info text-white" href="{{ url('/create') }}">レビュー投稿一覧へ</a>
                 </div>
-            </nav>
+            </div>
+        </div>
     </div>
+    
+    <div class="row ">
+        <div class="col-10 p-0 edit-post-box mx-auto">
 
-        <div class="post-wapper">
-            <div class="row">
-                <div class="col-sm-9 mx-auto">
-                    <div class="post-box">
-                        <div class="">
-                        <p class="text-center m-2 pt-2">
-                                ユーザー名
+                    <p class="text-center m-0 pt-2 h3">
+                        ユーザー名
+                    </p>
+                    <p class="text-center text-white bg-info p-2 m-0 h3">
+                        峠名: **&nbsp;&nbsp;&nbsp;&nbsp;地域: **
+                    </p>
+                    <p class="text-center m-2 pt-2 h3">
+                            総合距離: **&nbsp;&nbsp;&nbsp;&nbsp;総合時間: ** 
+                        </p>
+                    <img class="d-block edit-img mx-auto" width="400" alt="" src="/img/フリーアイコン.png">
+
+                    <div class="row">
+                        <div class="col-4 mx-auto">
+                            <p class="text-center p-3 h3">
+                                総合評価
+                                <br/>★★★★★
+                                <br/>難易度
+                                <br/>★★★★★
+                                <br/>路面状況
+                                <br/>★★★★★
+                                <br/>景色のよさ
+                                <br/>★★★★★
                             </p>
-                            <p class="text-center m-2">
-                                地域: ** 
-                                <br/>峠名: ** 
-                            </p>
-                            <img class="d-block mx-auto" width="500" alt="" src="/img/フリーアイコン.png">
-                            <div class="mx-auto">
+                        </div>
 
-                                <p class="text-center m-2 pt-2">
-                                    総合距離: **   総合時間: ** 
+                            <div class="col-8 text-left p-3">
+                                <p class="h3">
+                                <ins>コメント</ins>
                                 </p>
-
-                                <p class="text-center p-3">
-                                    総合評価 : ★★★★★
-                                    <br/>難易度 : ★★★★★
-                                    <br/>路面状況 : ★★★★★
-                                    <br/>景色のよさ : ★★★★★
+                                <p  class="h3">
+                                    *****
+                                    <br/>*****
+                                    <br/>*****
+                                    <br/>*****
                                 </p>
-
-                                <p class="text-center p-3">
-                                    コメント 
-                                    <br/>****＊
-                                    <br/>****＊
-                                    <br/>****＊
-                                    <br/>****＊
-                                </p>
-
                             </div>
-                        </div>
                     </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-sm-9 mx-auto">
-                    <form>
-                        <!-- コメント入力 -->
-                        <div class="form-group">
-                            <label>コメント</label>
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="コメントする"></textarea>
-                        </div>
-                        <!-- 送信ボタン -->
-                        <div class="text-center">
-                            <button type="submit" class="btn btn-info m-2">コメントを送信</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
 
         </div>
+    </div>
+
+    <div class="row">
+        <div class="col-8 mx-auto">
+            <form>
+                <div class="form-group">
+                    <label>コメント</label>
+                    <textarea class="form-control" rows="3" with=80% placeholder="コメントする"></textarea>
+                </div>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-info text-white m-3">コメントを送信</button>
+                </div>
+            </form>
+        </div>
+    </div>
 
 @endsection
