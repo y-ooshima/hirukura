28b812108479802842a66e41b253c4409c195b7b
y-ooshima <gozira.1kawa@gmail.com>
2021-04-15 17:58:29 +0900

    メイン画面の追加修正

--- resources/views/review/index.blade.php
+++ resources/views/review/index.blade.php
@@ -114,54 +114,62 @@
             </div>
     
         <div class="feature mt-5 mb-5">
-            <div class="row">
-                <div class="col-sm-8">
+            <div class="bg-white">
+                <div class="row">
+                    <div class="col">
                         <div class="text-content text-center">
                             <h2 class="float-contents-title">峠から検索！</h2>
                             <div><p>『ヒルクラ』は、全国の峠情報が掲載されています。
                             <br>ルートは？標高は？走りに行く前に峠情報をチェックをチェックしよう！</p>
                             </div>
                         </div>
-                </div>
-                <div  class="col-sm-4">
-                    <div class="image-content text-center">
-                        <img class="" alt="" src="/img/目的地アイコン1.png">
                     </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-4">
-                    <div class="image-content text-center">
-                        <img class="" alt="" src="/img/目的地アイコン2.png">
-                    </div>
-                </div>
-                <div class="col-sm-8">
-                    <div class="text-content text-center">
-                        <h2 class="float-contents-title">登った峠を記録しよう</h2>
-                        <div>
-                            <p>登った峠の難易度や絶景度などの評価もできます。
-                            <br>「どんな峠に登ってきたのか」「どう感じたのか」は、
-                            <br>あなたの自転車人生を振り返るポイントになるはず。
-                            <br>ルートの評価は他の人が登る際の参考にもなります。</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-8">
-                    <div class="text-content text-center">
-                        <h2 class="float-contents-title">思い出をシェアしよう</h2>
-                        <div>
-                            <p>峠は最高のリフレッシュ、時にはアクシデントなど、様々な顔を持っています。
-                            <br>『ヒルクラ』では、自転車乗りの目線で口コミを書くことができます。
-                            <br>写真だけでなく、「感動した」「辛かった」「いつ登った」など、
-                            <br>私たちだけが味わえる『体験』を残していこう。</p>
-                        </div>
-                    </div>
-                    </div>
-                    <div class="col-sm-4">
+                    <div class="col d-none d-md-inline">
                         <div class="image-content text-center">
-                            <img class="" alt="" src="/img/コミュニケーションアイコン.png">
+                            <img class="" alt="" src="/img/目的地アイコン1.png">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="bg-light my-2">
+                <div class="row">
+                    <div class="col d-none d-md-inline">
+                        <div class="image-content text-center">
+                            <img class="" alt="" src="/img/目的地アイコン2.png">
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="text-content text-center">
+                            <h2 class="float-contents-title">登った峠を記録しよう</h2>
+                            <div>
+                                <p>登った峠の難易度や絶景度などの評価もできます。
+                                <br>「どんな峠に登ってきたのか」「どう感じたのか」は、
+                                <br>あなたの自転車人生を振り返るポイントになるはず。
+                                <br>ルートの評価は他の人が登る際の参考にもなります。</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="bg-white">
+                <div class="row">
+                    <div class="col">
+                        <div class="text-content text-center">
+                            <h2 class="float-contents-title">思い出をシェアしよう</h2>
+                            <div>
+                                <p>峠は最高のリフレッシュ、時にはアクシデントなど、様々な顔を持っています。
+                                <br>『ヒルクラ』では、自転車乗りの目線で口コミを書くことができます。
+                                <br>写真だけでなく、「感動した」「辛かった」「いつ登った」など、
+                                <br>私たちだけが味わえる『体験』を残していこう。</p>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="col d-none d-md-inline">
+                            <div class="image-content text-center">
+                                <img class="" alt="" src="/img/コミュニケーションアイコン.png">
+                            </div>
                         </div>
                     </div>
                 </div>
