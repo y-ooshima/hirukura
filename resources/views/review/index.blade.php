23342820a39ccd1781d21ca22424d04512d25b14
y-ooshima <gozira.1kawa@gmail.com>
2021-04-18 22:52:12 +0900

    メインページの修正

--- resources/views/review/index.blade.php
+++ resources/views/review/index.blade.php
@@ -7,111 +7,93 @@
             <div class="col-12 p-0">
                 <div class="cath-text">
                     <img class="d-block mx-auto" width="100%" src="img/4452717_m.jpg" />
-                    <p>とっておきの峠を見つけよう</p>
+                    <p>情報をシェアして
+                    <br/>とっておきの峠を見つけよう</p>
                 </div>
             </div>
         </div>
 
         <div class="row postlist">
-                <div class="col-12 center-align  mt-2">
-                    <div class="text-outline hide-on-med-and-up">
-                        <p>ヒルクラは、
-                        <br>自転車好き・ヒルクライム好きが
-                        <br>それぞれの体験をもとに、
-                        <br>情報をシェアしあうサービスです。</p>
-                        
-                        <p>口コミ投稿を見ているだけで、
-                        <br>「週末どこに走りに行く？」を解決します。</p>
+            <div class="col-12 center-align mt-2">
+                <div class="text-outline hide-on-med-and-up">
+                    <p>自転車好き・ヒルクライム好きが
+                    <br>それぞれの体験をもとに、
+                    <br>情報をシェアしあうサービスです。</p>
+                    <p>口コミ投稿を見ているだけで、
+                    <br>「週末どこに走りに行く？」
+                    <br>を解決します。</p>
+                </div>
+            </div>
+        </div>
+    
+        <div class="row postlist"> 
+            <div class="col p-0 main-post-box rounded mx-auto">
+                <a class="" href="{{ url('/edit') }}">
+                    <p class="text-center m-2 pt-2">
+                        ユーザー名
+                    </p>
+                    <p class="text-left bg-secondary text-white p-2">
+                        峠名: ** 
+                    </p>
+                    <img class="d-block mx-auto" width="150" alt="" src="/img/フリーアイコン.png">
+                    <div class="mx-auto">
+                        <p class="text-left p-3">
+                            コメント 
+                            <br/>*****
+                            <br/>*****
+                            <br/>*****
+                        </p>
                     </div>
-                </div>
-
-            
-            
-                <div class="row mx-auto"> 
-                        <div class="col main-post-box rounded">
-                            <a class="" href="{{ url('/edit') }}">
-                                <p class="text-center m-2 pt-2">
-                                    ユーザー名
-                                </p>
-                                <p class="text-left m-2">
-                                    峠名: ** 
-                                </p>
-                                <img class="d-block mx-auto" width="200" alt="" src="/img/フリーアイコン.png">
-                                <div class="mx-auto">
-                                    <div class="row">
-
-                                        <div class="col-8">
-                                            <p class="text-left p-3">
-                                                コメント 
-                                                <br/>****＊
-                                                <br/>****＊
-                                                <br/>****＊
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    
-                        <div class="col p-0 main-post-box rounded d-none d-md-inline">
-                            <a class="" href="{{ url('/edit') }}">
-                                <p class="text-center m-2 pt-2">
-                                    ユーザー名
-                                </p>
-                                <p class="text-left m-2">
-                                    峠名: ** 
-                                </p>
-                                <img class="d-block mx-auto" width="200" alt="" src="/img/フリーアイコン.png">
-                                <div class="mx-auto">
-                                    <div class="row">
-
-                                        <div class="col-8">
-                                            <p class="text-left p-3">
-                                                コメント 
-                                                <br/>****＊
-                                                <br/>****＊
-                                                <br/>****＊
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-
-                        
-                        <div class="col p-0 main-post-box rounded d-none d-lg-inline">
-                            <a class="" href="{{ url('/edit') }}">
-                                <p class="text-center m-2 pt-2">
-                                    ユーザー名
-                                </p>
-                                <p class="text-left m-2">
-                                    峠名: ** 
-                                </p>
-                                <img class="d-block mx-auto" width="200" alt="" src="/img/フリーアイコン.png">
-                                <div class="mx-auto">
-                                    <div class="row">
-
-                                        <div class="col-8">
-                                            <p class="text-left p-3">
-                                                コメント 
-                                                <br/>****＊
-                                                <br/>****＊
-                                                <br/>****＊
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                </div>
+                </a>
             </div>
-            
 
-            <div class="row postlist">
-                <div class="col-12 text-center mb-3">
-                    <a type="button" class="btn btn-secondary" href="{{ url('/show') }}">投稿をもっと見る</a>
-                </div>
+            <div class="col p-0 main-post-box rounded mx-auto d-none d-md-block">
+                <a class="" href="{{ url('/edit') }}">
+                    <p class="text-center m-2 pt-2">
+                        ユーザー名
+                    </p>
+                    <p class="text-left bg-secondary text-white p-2">
+                        峠名: ** 
+                    </p>
+                    <img class="d-block mx-auto" width="150" alt="" src="/img/フリーアイコン.png">
+                    <div class="mx-auto">
+                        <p class="text-left p-3">
+                            コメント 
+                            <br/>*****
+                            <br/>*****
+                            <br/>*****
+                        </p>
+                    </div>
+                </a>
             </div>
+
+            <div class="col p-0 main-post-box rounded mx-auto d-none d-lg-block">
+                <a class="" href="{{ url('/edit') }}">
+                    <p class="text-center m-2 pt-2">
+                        ユーザー名
+                    </p>
+                    <p class="text-left bg-secondary text-white p-2">
+                        峠名: ** 
+                    </p>
+                    <img class="d-block mx-auto" width="150" alt="" src="/img/フリーアイコン.png">
+                    <div class="mx-auto">
+                        <p class="text-left p-3">
+                            コメント 
+                            <br/>*****
+                            <br/>*****
+                            <br/>*****
+                        </p>
+                    </div>
+                </a>
+            </div>
+
+        </div>
+
+        <div class="row postlist">
+            <div class="col-12 text-center mb-3">
+                <a type="button" class="btn btn-secondary" href="{{ url('/show') }}">投稿をもっと見る</a>
+            </div>
+        </div>
     
         <div class="feature mt-5 mb-5">
             <div class="bg-white">
@@ -119,8 +101,10 @@
                     <div class="col">
                         <div class="text-content text-center">
                             <h2 class="float-contents-title">峠から検索！</h2>
-                            <div><p>『ヒルクラ』は、全国の峠情報が掲載されています。
-                            <br>ルートは？標高は？走りに行く前に峠情報をチェックをチェックしよう！</p>
+                            <div>
+                                <p>全国の峠情報が掲載されています。
+                                <br>ルートは？標高は？走りに行く前に峠情報を
+                                <br>チェックをチェックしよう！</p>
                             </div>
                         </div>
                     </div>
@@ -143,10 +127,11 @@
                         <div class="text-content text-center">
                             <h2 class="float-contents-title">登った峠を記録しよう</h2>
                             <div>
-                                <p>登った峠の難易度や絶景度などの評価もできます。
-                                <br>「どんな峠に登ってきたのか」「どう感じたのか」は、
-                                <br>あなたの自転車人生を振り返るポイントになるはず。
-                                <br>ルートの評価は他の人が登る際の参考にもなります。</p>
+                                <p>登った峠の難易度や絶景度などの
+                                <br>評価もできます。
+                                <br>「どんな峠に登ったか」「どう感じたのか」は、
+                                <br>自転車人生を振り返るポイントになるはず。
+                                <br>他の人が登る際の参考にもなります。</p>
                             </div>
                         </div>
                     </div>
@@ -159,17 +144,16 @@
                         <div class="text-content text-center">
                             <h2 class="float-contents-title">思い出をシェアしよう</h2>
                             <div>
-                                <p>峠は最高のリフレッシュ、時にはアクシデントなど、様々な顔を持っています。
-                                <br>『ヒルクラ』では、自転車乗りの目線で口コミを書くことができます。
-                                <br>写真だけでなく、「感動した」「辛かった」「いつ登った」など、
-                                <br>私たちだけが味わえる『体験』を残していこう。</p>
+                                <p>自転車乗りの目線で口コミを書くことができます。
+                                <br>写真だけでなく、「感動した」「辛かった」
+                                <br>「いつ登った」など、私たちだけが味わえる
+                                <br>『体験』を残していこう。</p>
                             </div>
                         </div>
-                        </div>
-                        <div class="col d-none d-md-inline">
-                            <div class="image-content text-center">
-                                <img class="" alt="" src="/img/コミュニケーションアイコン.png">
-                            </div>
+                    </div>
+                    <div class="col d-none d-md-inline">
+                        <div class="image-content text-center">
+                            <img class="" alt="" src="/img/コミュニケーションアイコン.png">
                         </div>
                     </div>
                 </div>
@@ -177,10 +161,10 @@
         </div>
 
         <div class="row cv-area text-center">
-                <div class="col-12">
-                    <p class="cv-text text-outline">\登録してシェアしよう/</p>
-                    <a type="button" class="btn btn-secondary" href="{{ url('/login') }}">会員登録する</a>
-                </div>
+            <div class="col-12 mb-5">
+                <p class="cv-text text-outline">\登録してシェアしよう/</p>
+                <a type="button" class="btn btn-secondary" href="{{ url('/login') }}">会員登録する</a>
+            </div>
         </div>
     </div>
 
