7ba2a7561e930c17ded535069579639ba55830eb
y-ooshima <gozira.1kawa@gmail.com>
2021-04-24 22:49:08 +0900

    ログアウト機能の追加

--- resources/views/users/mypage.blade.php
+++ resources/views/users/mypage.blade.php
@@ -62,12 +62,11 @@
 
             <div class="row">
                 <div class="col text-center">
-                    <a href="{{ url('/') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                    ログアウト
+                    <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                        ログアウト
                     </a>
-
-                    <form id="logout-form" action="{{ url('/show') }}" method="POST" style="display: none;">
-                    @csrf
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
                     </form>
                 </div>
             </div>
