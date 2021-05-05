08288e7e803045f9a510eb92e0287aaae0ad8fa6
y-ooshima <gozira.1kawa@gmail.com>
2021-05-05 21:51:21 +0900

    不要ファイルの削除、post用モデル、コントローラの追加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,47 +13,6 @@ class UserController extends Controller
         $user = Auth::user();
         return view('users.mypage', compact('user'));
     }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function show(User $user)
-    {
-        //
-    }
 
     /**
      * Show the form for editing the specified resource.
@@ -86,17 +45,6 @@ class UserController extends Controller
         return redirect()->route('mypage');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(User $user)
-    {
-        //
-    }
-
     public function edit_password()
     {
         return view('users.edit_password');
@@ -115,12 +63,6 @@ class UserController extends Controller
 
         return redirect()->route('mypage');
     }
-
-    // public function delete(Request $request)
-    // {
-    //     $article = Auth::user($request->id);
-    //     return view('article.delete', ['article' => $article]); 
-    // }
 
     public function delete(Request $request)
     {
