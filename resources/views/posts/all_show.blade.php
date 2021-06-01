d9b8e558953bf001ac0b26c1cddc08a4e32955b2
y-ooshima <gozira.1kawa@gmail.com>
2021-06-01 22:50:47 +0900

    全体レビュー画面の修正

--- resources/views/posts/all_show.blade.php
+++ resources/views/posts/all_show.blade.php
@@ -32,7 +32,7 @@
 
     @foreach ($posts as $post)
         <div class="row mt-5"> 
-            <div class="col-12 card-deck mx-auto" style="width:auto">
+            <div class="col-8 card-deck mx-auto" style="width:auto">
                 <div class="col card" style="width:15rem;">
                     <h4 class="card-title text-center">峠名: {{ $post->id }} </h4>
                     <img class="card-img-top d-block edit-img mx-auto" src="/img/free_icon.png" alt="Card image cap">
